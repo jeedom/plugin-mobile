ee03ec404a9887c104bfaa316d56c7f4a79736e5
Rocket13011 <alexandre.orilia@gmail.com>
2016-08-22 16:13:20 +0200

    add objets

--- desktop/php/mobile.php
+++ desktop/php/mobile.php
@@ -29,7 +29,7 @@ foreach ($eqLogics as $eqLogic) {
    <ul class="nav nav-tabs" role="tablist">
 		<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipements}}</a></li>
 		<li role="presentation"><a href="#plugintab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Plugins}}</a></li>
-		<li role="presentation"><a href="#objecttab" aria-controls="profile" role="tab" data-toggle="tab"><i class="icon maison-house112"></i> {{Pièces}}</a></li>
+		<li role="presentation"><a href="#objecttab" aria-controls="profile" role="tab" data-toggle="tab"><i class="icon maison-house112"></i> {{Objets / Pièces}}</a></li>
 		<!--<li role="presentation"><a href="#scenariotab" aria-controls="profile" role="tab" data-toggle="tab"><i class="icon jeedom-clap_cinema"></i> {{Scénarios}}</a></li>-->
 	</ul>
 	<div class="tab-content">

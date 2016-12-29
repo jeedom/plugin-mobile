60474657ce04348c93e969ceaf5e385d8b3695c9
Rocket13011 <alexandre.orilia@gmail.com>
2016-12-29 13:56:11 +0100

    bugfix unset

--- core/api/mobile.api.php
+++ core/api/mobile.api.php
@@ -55,7 +55,7 @@ if ($jsonrpc->getMethod() == 'sync_homebridge') {
 	while($i <= count($eqLogics)){
 		if(isset($eqLogics[$i]["sendToHomebridge"])){
 			if($eqLogics[$i]["sendToHomebridge"] == 0){
-				unset($eqLogics, $i);	
+				unset($eqLogics[$i]);	
 			}
 		}
 	$i++;	

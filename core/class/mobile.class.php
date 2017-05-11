29acf2af19cd28ed580491812ef6d153225f8a3d
Rocket13011 <alexandre.orilia@gmail.com>
2017-05-11 15:04:42 +0200

    Change pollerperiod to 25

--- core/class/mobile.class.php
+++ core/class/mobile.class.php
@@ -150,7 +150,7 @@ class mobile extends eqLogic {
 		$plateform['name'] = "Jeedom";
 		$plateform['url'] = network::getNetworkAccess('internal');
 		$plateform['apikey'] = $apikey;
-		$plateform['pollerperiod'] = 5;
+		$plateform['pollerperiod'] = 25;
 		$response['platforms'] = array();
 		$response['platforms'][] = $plateform;
 		exec('sudo chown -R www-data:www-data ' . dirname(__FILE__) . '/../../resources');

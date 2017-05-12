609baf818223131f5811811f038c3329a72d2e0c
Rocket13011 <alexandre.orilia@gmail.com>
2017-05-12 15:30:07 +0200

    -eq instead of ==

--- core/class/mobile.class.php
+++ core/class/mobile.class.php
@@ -192,12 +192,12 @@ class mobile extends eqLogic {
 		}
 		
 		// check dbus started, if not, start
-		$cmd = 'if [ $(ps -ef | grep -v grep | grep "dbus-daemon" | wc -l) == 0 ]; then sudo systemctl start dbus;echo "Starting dbus because not started"; fi';
+		$cmd = 'if [ $(ps -ef | grep -v grep | grep "dbus-daemon" | wc -l) -eq 0 ]; then sudo systemctl start dbus;echo "Starting dbus because not started"; fi';
 		log::add('mobile_homebridge', 'info', 'Vérification dbus : ' . $cmd);
 		exec($cmd . ' >> ' . log::getPathToLog('mobile_homebridge') . ' 2>&1 &');
 		
 		// check avahi-daemon started, if not, start
-		$cmd = 'if [ $(ps -ef | grep -v grep | grep "avahi-daemon" | wc -l) == 0 ]; then sudo systemctl start avahi-daemon;echo "Starting avahi-daemon because not started"; fi';
+		$cmd = 'if [ $(ps -ef | grep -v grep | grep "avahi-daemon" | wc -l) -eq 0 ]; then sudo systemctl start avahi-daemon;echo "Starting avahi-daemon because not started"; fi';
 		log::add('mobile_homebridge', 'info', 'Vérification avahi-daemon : ' . $cmd);
 		exec($cmd . ' >> ' . log::getPathToLog('mobile_homebridge') . ' 2>&1 &');
 				
@@ -220,7 +220,7 @@ class mobile extends eqLogic {
 		message::removeAll('homebridge', 'unableStartDeamon');
 		log::add('mobile_homebridge', 'info', 'Démon homebridge lancé');
 		// Check if IPv6 is enable, and display a warning if it is
-		$cmd = 'if [ $(netstat -na | grep 51826 | grep tcp6 | wc -l) ]; then echo "WARNING : IPv6 est activé sur votre système, il peut poser problème avec Homebridge"; fi';
+		$cmd = 'if [ $(netstat -na | grep 51826 | grep "tcp6" | wc -l) -gt 0 ]; then echo "WARNING : IPv6 est activé sur votre système, il peut poser problème avec Homebridge"; fi';
 		log::add('mobile_homebridge', 'info', 'Vérification IPv6 : ' . $cmd);
 		exec($cmd . ' >> ' . log::getPathToLog('mobile_homebridge') . ' 2>&1 &');
 	}

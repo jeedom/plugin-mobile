bcc91787e498dd1d99ec82473eae223641dbb6d8
Rocket13011 <alexandre.orilia@gmail.com>
2017-06-16 11:27:10 +0200

    install dependancy update Plugin mobile (iOS Only)

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -1,7 +1,35 @@
+<?php
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+
 function mobile_update(){
+	$ios = 0;
     	foreach (eqLogic::byType('mobile') as $mobile){
-			if($mobile->getLogicalId() == null || $mobile->getLogicalId() == ""){
-				$mobile->remove();
+		if($mobile->getLogicalId() == null || $mobile->getLogicalId() == ""){
+			$mobile->remove();
+		}else{
+			if($mobile->getConfiguration('type_mobile') == "ios"){
+				$ios = 1;
 			}
 		}
+	}
+	if($ios == 1){
+		mobile::dependancy_install();
+		mobile::deamon_start();
+	}
 }
+?>

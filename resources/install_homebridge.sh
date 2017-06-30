efe9e1f3959c68474f26741e4ed643dc763c68e1
Rocket13011 <alexandre.orilia@gmail.com>
2017-06-30 11:03:26 +0200

    delete allow-interfaces

--- resources/install_homebridge.sh
+++ resources/install_homebridge.sh
@@ -80,7 +80,7 @@ sudo sed -i "/.*enable-dbus.*/c\enable-dbus=yes  #change by homebridge" /etc/ava
 sudo sed -i "/.*use-ipv6.*/c\use-ipv6=no  #change by homebridge" /etc/avahi/avahi-daemon.conf
 if [ -n $1 ]; then
 	UsedEth=$(ifconfig | awk '/'$1'/ {print $1}' RS="\n\n")
-	sudo sed -i "/.*allow-interfaces.*/c\allow-interfaces=$UsedEth  #change by homebridge" /etc/avahi/avahi-daemon.conf
+	sudo sed -i "/.*allow-interfaces.*/c\#allow-interfaces=$UsedEth  #change by homebridge" /etc/avahi/avahi-daemon.conf
 fi
 echo "Redémarrage avahi-daemon..."
 sudo systemctl restart avahi-daemon

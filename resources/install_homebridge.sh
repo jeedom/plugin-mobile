766756e946e41404025b32c1ebc94c8239db9975
Rocket13011 <alexandre.orilia@gmail.com>
2017-07-07 09:51:46 +0200

    add Redémarrage avahi-daemon

--- resources/install_homebridge.sh
+++ resources/install_homebridge.sh
@@ -76,14 +76,12 @@ if [ $? -ne 0 ]; then
 	echo "avahi-daemon non activé au démarrage, activation..."
 	sudo systemctl enable avahi-daemon
 fi
-sudo sed -i "/.*enable-dbus.*/c\enable-dbus=yes  #change by homebridge" /etc/avahi/avahi-daemon.conf
-sudo sed -i "/.*use-ipv6.*/c\use-ipv6=no  #change by homebridge" /etc/avahi/avahi-daemon.conf
+sudo sed -i "/.*enable-dbus.*/c\enable-dbus=yes  #changed by homebridge" /etc/avahi/avahi-daemon.conf
+sudo sed -i "/.*use-ipv6.*/c\use-ipv6=no  #changed by homebridge" /etc/avahi/avahi-daemon.conf
 if [ -n $1 ]; then
 	UsedEth=$(ifconfig | awk '/'$1'/ {print $1}' RS="\n\n")
-	sudo sed -i "/.*allow-interfaces.*/c\#allow-interfaces=$UsedEth  #change by homebridge" /etc/avahi/avahi-daemon.conf
+	sudo sed -i "/.*allow-interfaces.*/c\#allow-interfaces=$UsedEth  #changed by homebridge" /etc/avahi/avahi-daemon.conf
 fi
-echo "Redémarrage avahi-daemon..."
-sudo systemctl restart avahi-daemon
 echo "Installation Homebridge OK"
 echo 100 > /tmp/homebridge_in_progress
 echo "--100%"

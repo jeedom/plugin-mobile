6e35e545ff62c7ca50d1f870b5a0b7beb44ab024
Rocket13011 <alexandre@jeedom.com>
2017-10-17 11:33:15 +0200

    nettoyage Master

--- plugin_info/install.php
+++
@@ -1,7 +0,0 @@
-function mobile_update(){
-    	foreach (eqLogic::byType('mobile') as $mobile){
-			if($mobile->getLogicalId() == null || $mobile->getLogicalId() == ""){
-				$mobile->remove();
-			}
-		}
-}

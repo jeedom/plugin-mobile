257cd87a1457e015737539b9e905e3104b09a743
Rocket13011 <alexandre@jeedom.com>
2018-06-11 17:56:17 +0200

    ajout du saut a la ligne dans les notifs

--- core/class/mobile.class.php
+++ core/class/mobile.class.php
@@ -399,7 +399,7 @@ class mobile extends eqLogic {
 
 	public static function jsonPublish($os, $titre, $message, $badge = 'null', $type, $idNotif, $answer, $timeout) {
 		$dateNotif = date("Y-m-d H:i:s");
-		$message = preg_replace("# {2,}#"," ",preg_replace("#(\r\n|\n\r|\n|\r)#"," ",$message));
+		$message = preg_replace("# {2,}#"," ",preg_replace("#(\r\n|\n\r|\n|\r)#","\\\\\\n",$message));
 		if ($timeout != 'nok') {
 			$timeout = date('Y-m-d H:i:s', strtotime("$dateNotif + $timeout SECONDS"));
 		}

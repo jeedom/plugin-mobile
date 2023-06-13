feda80ee91f88a0adb92d577d9a9c8ec261f842d
Sekiro-kost <julien@jeedom.com>
2023-06-13 10:05:38 +0200

    fix searchBar

--- desktop/php/mobile.php
+++ desktop/php/mobile.php
@@ -47,7 +47,8 @@ $plugin_widget = mobile::$_pluginWidget;
 			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
 			<div class="input-group-btn">
 				<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>
-				<a class="btn hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
+			
+				<a class="btn hidden roundedRight" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
 			</div>
 		</div>
 
@@ -110,13 +111,7 @@ $plugin_widget = mobile::$_pluginWidget;
 
 		</div>
 		<legend><i class="icon techno-listening3"></i> {{Mes Téléphones Mobiles}}</legend>
-		<div class="input-group" style="margin:5px;">
-			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
-			<div class="input-group-btn">
-				<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>
-				<a class="btn hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
-			</div>
-		</div>
+
 
 		<div class="eqLogicThumbnailContainer">
 			<?php

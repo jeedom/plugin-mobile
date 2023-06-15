46fce51d96fbf5af8b9eaf14aa0d708c48b2b354
Rocket13011 <1051575+rocket13011@users.noreply.github.com>
2023-06-15 16:01:16 +0200

    nouvelle version APNS

--- core/class/mobile.class.php
+++ core/class/mobile.class.php
@@ -668,35 +668,40 @@ class mobile extends eqLogic {
                'priority' => 'high',
               	];
 
-
-				  $apns = [
-					'headers' => [
-						'apns-priority' => '10',
-						'apns-collapse-id' => strval($idNotif),
-					],
-					 'payload' => [
-						'aps' => [
-							'mutableContent' => false,
-							'content-available' => true,
-						],
-						'notifee_options' => [
-							'ios' => [
-							'sound' => 'default',
-							'critical' => $critical,
-							'foregroundPresentationOptions' => [
-								'alert' => true,
-								'badge' => true,
-								'sound' => true
-								]
-							]
+			$apns = [
+			'headers' => [
+				'apns-priority' => '10',
+				'apns-collapse-id' => strval($idNotif),
+				'apns-push-type' => 'alert',
+				'apns-topic' => '4G374KJ6HT.com.jeedom.jeedomobile',
+			],
+			'payload' => [
+				'aps' => [
+					'mutable-content' => false,
+					'content-available' => true,
+					'sound' => [
+						'name' => 'default',
+						'critical' => $critical
+					]
+				],
+				'notifee_options' => [
+					'ios' => [
+					'sound' => 'default',
+					'critical' => $critical,
+					'foregroundPresentationOptions' => [
+						'alert' => true,
+						'badge' => true,
+						'sound' => true
 						]
-					  ]
-				   ];
+					]
+				]
+				]
+			];
 
 
               if($photo != null){
                  $android['data']['image'] = $photo;
-				 //$notification['image'] = $photo;
+				 $notification['image'] = $photo;
 				 $apns['payload']['aps']['launch-image'] = $photo;
                  $apns['payload']['notifee_options']['image'] = $photo;
                  $apns['payload']['notifee_options']['ios']['attachments'] = [
@@ -718,7 +723,7 @@ class mobile extends eqLogic {
 				  'token' => $token,
 				  'data' => $data,
 				  'apns' => $apns,
-				  'notification' => $notification,
+				  'notification' => $notification
 				];
 			}
 	     }

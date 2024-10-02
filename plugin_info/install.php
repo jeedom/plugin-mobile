cdc17a9538f2136e9c9d64d8f155a3666782a32c
David <79108364+Phpvarious@users.noreply.github.com>
2024-10-03 00:13:13 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,21 +18,41 @@ require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function mobile_install()
 {
-	$oldFiles = [dirname(__FILE__) . '/../desktop/css/panel.css', 
-				 dirname(__FILE__) . '/../desktop/php/panelMenuCustom.php',
-				 dirname(__FILE__) . '/../desktop/js/panelMenuCustom.js',
-				 dirname(__FILE__) . '/../desktop/modal/health.php'];
+	//config::save('displayMobilePanel',1, 'mobile');
+  
+	/* Create folder for notifications */  
+	$pathNotifications = dirname(__FILE__) . '/../core/data/notifications/';
+    if(!is_dir($pathNotifications)){
+        mkdir($pathNotifications, 0775, true);
+    }
+  
+	/* Delete old files of plugin */
+	$oldFiles = [
+		dirname(__FILE__) . '/../desktop/css/panel.css', 
+		dirname(__FILE__) . '/../desktop/php/panelMenuCustom.php',
+		dirname(__FILE__) . '/../desktop/js/panelMenuCustom.js',
+		dirname(__FILE__) . '/../desktop/modal/health.php'
+		dirname(__FILE__) . '/../desktop/modal/modal.previousMenus.php'
+		dirname(__FILE__) . '/../desktop/modal/plugin.php'
+		dirname(__FILE__) . '/../desktop/modal/piece.php'
+		dirname(__FILE__) . '/../desktop/modal/scenario.php'
+		dirname(__FILE__) . '/../desktop/modal/info_app.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/plugin.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/object.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/scenario.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/menuCustom.php'
+		dirname(__FILE__) . '/../desktop/modal/update.mobile.php'
+	];
 	foreach ($oldFiles as $oldFile) {
 		if (file_exists($oldFile)) {
 			shell_exec('rm ' . $oldFile);
 		} 		
 	}
-	//config::save('displayMobilePanel',1, 'mobile');
+
+	/* Generate ApiKey if no exist */
 	jeedom::getApiKey('mobile');
-	$pathNotifications = dirname(__FILE__) . '/../core/data/notifications/';
-    if(!is_dir($pathNotifications)){
-        mkdir($pathNotifications, 0775, true);
-    }
+  
+	
 }
 
 
@@ -40,6 +60,8 @@ function mobile_install()
 function mobile_update()
 {
 	//	config::save('displayMobilePanel',1, 'mobile');
+  
+	/* Create folder for notifications */
 	$pathNotifications = dirname(__FILE__) . '/../core/data/notifications/';
     if(!is_dir($pathNotifications)){
         mkdir($pathNotifications, 0775, true);
@@ -47,35 +69,61 @@ function mobile_update()
 	
 	$mobiles = eqLogic::byType('mobile');
 	foreach($mobiles as $mobile){
+		/* Delete mobile with bad logicalId */
+		if ($mobile->getLogicalId() == null || $mobile->getLogicalId() == "") {
+			$mobile->remove();
+			continue;
+		}     
+		/* Set menu by defaut if no exist */
 		$customMenu  = $mobile->getConfiguration('menuCustomArray');
-		if(!isset($customMenu)){	
-			$icons = $mobile->getConfiguration('nbIcones', 3);
-			$menuCustomArray = array();
-			for($i=1; $i < intval($icons) + 1; $i++){
-				$menuCustomArray[$i]['selectNameMenu'] = $mobile->getConfiguration('selectNameMenu'.$i);
-				$menuCustomArray[$i]['renameIcon'] = $mobile->getConfiguration('renameIcon'.$i);
-				$menuCustomArray[$i]['spanIcon'] = $mobile->getConfiguration('spanIcon'.$i);
-				$menuCustomArray[$i]['urlUser'] = $mobile->getConfiguration('urlUser'.$i);
-			}
+		if(empty($customMenu)){
+			$menuCustomArray = mobile::getMenuDefaultV2();
+			$mobile->setConfiguration('nbIcones', count($menuCustomArray));
+			$mobile->setConfiguration('defaultIdMobile', $mobile->getId());
 			$mobile->setConfiguration('menuCustomArray', $menuCustomArray);
 			$mobile->save();
 		}
 	}
-	$oldFiles = [dirname(__FILE__) . '/../desktop/css/panel.css', 
-	             dirname(__FILE__) . '/../desktop/php/panelMenuCustom.php',
-							 dirname(__FILE__) . '/../desktop/js/panelMenuCustom.js',
-							 dirname(__FILE__) . '/../desktop/modal/health.php'];
+  
+	/* Delete old "menuCustom_" and "NoCut" save into config of plugin */
+	foreach(config::searchKey('menuCustom_', 'mobile') as $configMenuCustom) {
+		config::remove($configMenuCustom['key'], 'mobile');
+	}
+	foreach(config::searchKey('NoCut', 'mobile') as $iconNoCut) {
+		config::remove($iconNoCut['key'], 'mobile');
+	}
+  
+	/* Delete old "previousMenus" save into config of plugin */
+	config::remove('previousMenus', 'mobile');
+	config::remove('pluginPanelOutMobile', 'mobile');
+  
+	/* Delete old files of plugin */
+	$oldFiles = [
+		dirname(__FILE__) . '/../desktop/css/panel.css', 
+		dirname(__FILE__) . '/../desktop/php/panelMenuCustom.php',
+		dirname(__FILE__) . '/../desktop/js/panelMenuCustom.js',
+		dirname(__FILE__) . '/../desktop/modal/health.php'
+		dirname(__FILE__) . '/../desktop/modal/modal.previousMenus.php'
+		dirname(__FILE__) . '/../desktop/modal/plugin.php'
+		dirname(__FILE__) . '/../desktop/modal/piece.php'
+		dirname(__FILE__) . '/../desktop/modal/scenario.php'
+		dirname(__FILE__) . '/../desktop/modal/info_app.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/plugin.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/object.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/scenario.mobile.php'
+		dirname(__FILE__) . '/../desktop/modal/menuCustom.php'
+		dirname(__FILE__) . '/../desktop/modal/update.mobile.php'
+	];
 	foreach ($oldFiles as $oldFile) {
 		if (file_exists($oldFile)) {
 			shell_exec('rm ' . $oldFile);
 		} 		
 	}
-
+  
+	/* Generate ApiKey if no exist */
 	jeedom::getApiKey('mobile');
-	foreach (eqLogic::byType('mobile') as $mobile) {
-		if ($mobile->getLogicalId() == null || $mobile->getLogicalId() == "") {
-			$mobile->remove();
-		}
-	}
-	mobile::makeTemplateJson();
+  
+	/* Make template in json format */
+	/* V1 ?
+    mobile::makeTemplateJson();
 }
